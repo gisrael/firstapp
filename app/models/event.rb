df71c65801e84cd7ce71f3949e6f98681e431352
Israel R. Geselowitz <gisrael@seas.upenn.edu>
2012-11-05 21:19:13 -0500

    Finished assignment

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :location, :name, :time, :group, :group_id
   belongs_to :group
+  scope :completed_event, where('date < ?', Date.today)
+  scope :upcoming_event, where('date > ?', Date.today)
 end
